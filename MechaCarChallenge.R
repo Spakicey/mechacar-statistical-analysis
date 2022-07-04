4376cbecdd25dc702b207ad02c0209c21f5dd4d2
Zack <zackvalis@gmail.com>
2022-07-03 22:02:51 -0400

    Update file with t-test code

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -17,10 +17,17 @@ total_summary <- susCoil %>% summarize(Mean=mean(PSI), Median=median(PSI), Varia
 # Create lot summary using group_by and summarize
 lot_summary <- susCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups='keep')
 
+# Comparing all lots against mean PSI of the population w/ t-test
+t.test(susCoil$PSI,mu=mean(susCoil$PSI))
 
+# Comparing Lot 1 against mean population PSI
+Lot1_Data <- susCoil %>% filter(Manufacturing_Lot=="Lot1")
+t.test(Lot1_Data$PSI,mu=mean(susCoil$PSI))
 
+# Comparing Lot 2 against mean population PSI
+Lot2_Data <- susCoil %>% filter(Manufacturing_Lot=="Lot2")
+t.test(Lot2_Data$PSI,mu=mean(susCoil$PSI))
 
-
-
-
-
+# Comparing Lot 3 against mean population PSI
+Lot3_Data <- susCoil %>% filter(Manufacturing_Lot=="Lot3")
+t.test(Lot3_Data$PSI,mu=mean(susCoil$PSI))
